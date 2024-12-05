e97307634544ec1c1f5004246e3ad47b15b7968f
Romy-OR <131908@epsstudents.org>
2024-12-05 14:32:31 -0500

    Update song.java

--- song.java
+++ song.java
@@ -1,7 +1,41 @@
 public class Song
 {
-    public Song(){
-        
+    String title;
+    String artist;
+    String album;
+    String year;
+    
+    public Song(String Title, String Artist, String Album, String Year)
+    {
+        this.title = Title;
+        this.artist = Artist;
+        this.album = Album;
+        this.year = Year;
     }
     
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getArtist(){
+        return artist;
+    }
+    
+    public String getAlbum(){
+        return album;
+    }
+    
+    public String getYear(){
+        return year;
+    }
+    
+    public String toString()
+    {
+        if (album.equals("none")){
+            return "Artist: " + artist + "\nTitle: " + title + "\nNon-album single";
+        }
+        else{
+            return "Artist: " + artist + "\nTitle: " + title + "\nAlbum: " + album;
+        }
+    }
 }
